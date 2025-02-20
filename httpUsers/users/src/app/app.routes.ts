014b200fffd3012b892ded3b4bd7d6ba10ce84aa
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-20 20:45:54 +0100

    routing

--- httpUsers/users/src/app/app.routes.ts
+++ httpUsers/users/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { UserListComponent } from './user-list/user-list.component';
 import { ContactComponent } from './contact/contact.component';
+import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
-    { path: 'userList', component: UserListComponent },
+    { path: 'users', component: UsersComponent },
     { path: 'contact', component: ContactComponent},
 ];
