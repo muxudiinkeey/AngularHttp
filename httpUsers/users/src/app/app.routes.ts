acf2ac96363314582a5322190fbffdcd78ad65d3
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-22 00:42:23 +0100

    table ok

--- httpUsers/users/src/app/app.routes.ts
+++ httpUsers/users/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
     { path: 'users', component: UsersComponent },
-    { path: 'contact', component: ContactComponent},
 ];
