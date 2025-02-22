8413ae0cef29706dd138f4e8378cf754afbcfabc
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-22 02:06:06 +0100

    isticmaalay next

--- httpUsers/users/src/app/home/home.component.ts
+++ httpUsers/users/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../users/user.service';
+import { Iusers } from '../users/user';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+    userservice = inject (UserService)
+  
+    users: Iusers[]= []
+
+    constructor(){
+      this.userservice.getUsers().subscribe({
+    next:(data)=>{
+      this.users = data;
+    },
+    error:(err)=>{
+ console.log(err);
+    }
+      });
+    }
+  
+ /*    ngOnInit(): void {
+      this.onGitUsers();
+    } */
+   users$ = this.userservice.getUsers();
+  onGitUsers(){
+    this.userservice.getUsers().subscribe((Iusers)=>{
+      this.users= this.users
+    })
+  }
 
 }
