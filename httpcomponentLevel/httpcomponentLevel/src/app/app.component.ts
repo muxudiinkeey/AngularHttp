997446aee77cfd418d96cbd65dff534905805d6c
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-25 01:02:01 +0100

    ok

--- httpcomponentLevel/httpcomponentLevel/src/app/app.component.ts
+++ httpcomponentLevel/httpcomponentLevel/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Iuser } from './user';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +15,22 @@ export class AppComponent {
 private url='http://jsonplaceholder.typicode.com/posts';
 
 http = inject(HttpClient);
+user : any;
+users : Iuser []= []
+
 
 $posts = this.getPosts();
 // GET
 getPosts(){
-  return this.http.get<any>(this.url)
+  return this.http.get<Iuser[]>(this.url)
 }
 
 getPost(id: number ){
 
-  return this.http.get(this.url+'/'+id);
+  return this.http.get<Iuser[]>(this.url+'/'+id);
 }
 
-// CREATE
+/* // CREATE
 greatePost(post: any){
  return this.http.post(this.url, post);
 }
@@ -36,11 +40,11 @@ greatePost(post: any){
 updatePost(post: any){
  return this.http.put(this.url+'/'+ post.id, post);
 }
-
-// DELETE
+} */
+ 
 deletePost(id: number){
   return this.http.delete(this.url+'/'+id);
-}
 
+}
 
 }
