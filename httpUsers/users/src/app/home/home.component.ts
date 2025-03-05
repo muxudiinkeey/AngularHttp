1a7273a636b65e40fb58215afb2589606f7d274c
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-05 16:48:40 +0100

    ok

--- httpUsers/users/src/app/home/home.component.ts
+++ httpUsers/users/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { Iuser} from '../users/user';
-import { HomeService } from './home.service';
+import { UserService } from '../users/user.service';
+import { Iuser } from '../users/user';
 
 @Component({
   selector: 'app-home',
@@ -9,46 +9,28 @@ import { HomeService } from './home.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  user: any;
-  Users : Iuser[]=[]
-  homeService = inject(HomeService)
+userservice = inject (UserService)
+
+  users: Iuser[]= []
+
+  ngOnInit(): void {
+    this.onGitUsers();
+  }
+  // asynk hadaad isticmaalaysid dollor users isticmaal
+   users$ = this.userservice.getUsers();
+
+onGitUsers(){
+  this.userservice.getUsers().subscribe((Iuser)=>{
+
+    //this.users= this.users
+    this.users= Iuser;
+  })
+}
  
- ngOnInit(): void {
- /*     this.onGetusers();
-     this.onGetuser();
-      */
- }
+
  
-   onGetusers(){
-     this.homeService.getUsers().subscribe(
-       (response) => console.table(response),
-       
-       (error:any) =>console.warn(error),
-       ()=> console.log('liiska oo dhan users')
-     )
-   }
-   onGetuser(){
-     this.homeService.getUser().subscribe(
-       (response) => console.info(response),
-       (error:any) =>console.warn(error),
-       ()=> console.log('Waa hal user')
-     )
-   }
-   onCreateUsser(user: Iuser){
-     this.homeService.createUser(this.user).subscribe(
-       (Users) => console.table(Users),
-       (error:any) =>console.warn(error),
-       ()=> console.log('Done creating user')
-     )
-   }
-   onUpdateUsser(user: Iuser){
-     this.homeService.updateUser(this.user).subscribe(
-       (Users) => console.table(Users),
-       (error:any) =>console.warn(error),
-       ()=> console.log('Done creating user')
-     )
    }
 
 
@@ -92,6 +74,6 @@ export class HomeComponent implements OnInit {
     }
    */
   
-  }
+  
 
 
