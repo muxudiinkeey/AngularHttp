e110679db0ec3fa689717abcc1a6de7f16e40f3d
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-12 19:35:34 +0100

    adeed view update

--- httpUsers/users/src/app/app.routes.ts
+++ httpUsers/users/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { GetUserComponent } from './get-user/get-user.component';
-import { EditComponent } from './edit/edit.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'users' },
     { path: 'home', component: HomeComponent },
     { path: 'users', component: UsersComponent },
     { path: 'addUser', component: AddUserComponent },
-    { path: 'GetUser', component: GetUserComponent },
-    { path: 'updateUser', component: EditComponent },
+    { path: 'getUser', component: GetUserComponent },
+    { path: 'updateUser', component: UpdateUserComponent},
+    { path: 'viewUser', component: ViewUserComponent},
 ];
