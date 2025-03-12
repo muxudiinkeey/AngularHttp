850081c007b54c03b1a50dd7c79bff904fc3cfa2
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-12 16:08:18 +0100

    ok

--- httpUsers/users/src/app/add-user/add-user-service.service.ts
+++ httpUsers/users/src/app/add-user/add-user-service.service.ts
@@ -10,11 +10,5 @@ export class AddUserServiceService {
   // http api users
   http = inject(HttpClient);
 
-  
-/* 
-  private Url = 'https://jsonplaceholder.typicode.com/users';
-  //users : Iusers [] = []
-  addUser(user: Iuser): Observable<Iuser> {
-    return this.http.put<Iuser>(`${this.Url}/${user.id}`, user);
-  } */
+
 }
